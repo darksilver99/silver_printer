b5709c0722c540708c6bdf61534a8c79297b96a7
Darksilver88 <anusorn@koder3.com>
2025-07-20 16:58:14 +0700

    a

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -728,6 +728,7 @@ class _PrintPreviewScreenState extends State<PrintPreviewScreen> {
             settings: {
               'density': 'medium',
               'alignment': 'center',
+              'feedLines': 3,
             },
           );
 
